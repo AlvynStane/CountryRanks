a818046bfefa6a697cf80b6a8a51bbc78fa10ee7
AlvynStane <211110583@students.mikroskil.ac.id>
2024-12-18 23:48:03 +0700

    edit vercel and vite

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,4 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://restcountries.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
